d9e443cd5d54ed44731fed957c5b93d8221786d1
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-12 15:48:20 -0300

    test: adicionou teste do requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "nome_do_produto",
+        "nome_da_empresa",
+        "12/08/2022",
+        "18/09/2023",
+        "111111",
+        "instrucoes",
+    )
+
+    assert product.id is int(1)
+    assert product.nome_do_produto is str("nome_do_produto")
+    assert product.nome_da_empresa is str("nome_da_empresa")
+    assert product.data_de_fabricacao is str("12/08/2022")
+    assert product.data_de_validade is str("18/09/2023")
+    assert product.numero_de_serie is str("111111")
+    assert product.instrucoes_de_armazenamento is str("instrucoes")
