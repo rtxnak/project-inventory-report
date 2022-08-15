b9c13f6a2ab61357de2a866e74b99a76acd59b73
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-15 17:08:28 -0300

    test: adicionou teste do requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "nome_do_produto",
+        "nome_da_empresa",
+        "12/08/2022",
+        "18/09/2023",
+        "111111",
+        "instrucoes",
+    )
+
+    assert product.__repr__() == (
+        "O produto nome_do_produto"
+        " fabricado em 12/08/2022"
+        " por nome_da_empresa com validade"
+        " até 18/09/2023"
+        " precisa ser armazenado instrucoes."
+    )
