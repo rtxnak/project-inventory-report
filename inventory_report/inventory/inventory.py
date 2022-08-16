38167a14b992bf1d634ffb5f84f4cc640f06dde5
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-16 01:06:39 -0300

    feat: adicionou requisito 7

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -7,10 +8,15 @@ class Inventory:
     @classmethod
     def import_data(self, path, type):
         list = []
-        with open(path) as file:
-            report = csv.DictReader(file, delimiter=",", quotechar='"')
-            for row in report:
-                list.append(row)
+        if path.endswith(".csv"):
+            with open(path) as file:
+                report = csv.DictReader(file, delimiter=",", quotechar='"')
+                for row in report:
+                    list.append(row)
+        if path.endswith(".json"):
+            with open(path) as file:
+                content = file.read()
+                list = json.loads(content)
         if type == "simples":
             return SimpleReport.generate(list)
         else:
