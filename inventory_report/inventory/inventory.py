d7a90d277ec68bbd37d637ef7d1e852d7d2055b2
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-16 01:36:40 -0300

    refact: refatoracao para reduzir complexidade da funcao

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,26 +6,42 @@ from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
+    def read_csv(path):
+        list = []
+        with open(path) as file:
+            report = csv.DictReader(file, delimiter=",", quotechar='"')
+            for row in report:
+                list.append(row)
+        return list
+
+    def read_json(path):
+        list = []
+        with open(path) as file:
+            content = file.read()
+            list = json.loads(content)
+        return list
+
+    def read_xml(path):
+        list = []
+        root = ET.parse(path).getroot()
+        for record in root:
+            item_dict = {}
+            for item in record:
+                item_dict[item.tag] = item.text
+            list.append(item_dict)
+        return list
+
+    def read(path):
+        if path.endswith(".csv"):
+            return Inventory.read_csv(path)
+        elif path.endswith(".json"):
+            return Inventory.read_json(path)
+        elif path.endswith(".xml"):
+            return Inventory.read_xml(path)
+
     @classmethod
     def import_data(self, path, type):
-        list = []
-        if path.endswith(".csv"):
-            with open(path) as file:
-                report = csv.DictReader(file, delimiter=",", quotechar='"')
-                for row in report:
-                    list.append(row)
-        if path.endswith(".json"):
-            with open(path) as file:
-                content = file.read()
-                list = json.loads(content)
-        if path.endswith(".xml"):
-            root = ET.parse(path).getroot()
-            for record in root:
-                item_dict = {}
-                for item in record:
-                    item_dict[item.tag] = item.text
-                list.append(item_dict)
         if type == "simples":
-            return SimpleReport.generate(list)
+            return SimpleReport.generate(self.read(path))
         else:
-            return CompleteReport.generate(list)
+            return CompleteReport.generate(self.read(path))
