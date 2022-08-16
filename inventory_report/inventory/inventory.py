dea17a733a705c8137c453a31335fc7993dc6e38
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-16 01:21:28 -0300

    feat: adicionou requisito 7

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from xml.etree import ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -17,6 +18,13 @@ class Inventory:
             with open(path) as file:
                 content = file.read()
                 list = json.loads(content)
+        if path.endswith(".xml"):
+            root = ET.parse(path).getroot()
+            for record in root:
+                item_dict = {}
+                for item in record:
+                    item_dict[item.tag] = item.text
+                list.append(item_dict)
         if type == "simples":
             return SimpleReport.generate(list)
         else:
