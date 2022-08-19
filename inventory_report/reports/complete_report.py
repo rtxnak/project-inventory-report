2c808890e17583c4154eb1675019ebe2904e4d04
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-18 22:20:46 -0300

    refactor: refatoracao do metodo products_by_company

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -3,13 +3,21 @@ from inventory_report.reports.simple_report import SimpleReport
 
 class CompleteReport(SimpleReport):
     def products_by_company(products):
-        products_by_companies_name = {
-            product["nome_da_empresa"]: 0 for product in products
-        }
+        value = 0
+        company_list = []
+
         for product in products:
-            products_by_companies_name[product["nome_da_empresa"]] += 1
+            if product["nome_da_empresa"] not in company_list:
+                company_list.append(product["nome_da_empresa"])
+
+        company_dict = dict.fromkeys(company_list, value)
+
+        for product in products:
+            company_dict[product["nome_da_empresa"]] += 1
+
         products_by_company = ""
-        for company, quantity in products_by_companies_name.items():
+
+        for company, quantity in company_dict.items():
             products_by_company += f"- {company}: {quantity}\n"
         return products_by_company
 
